8d25f80c4b406127bc3f5523609da93f994160ee
nautilX <mathewjoethomas@gmail.com>
2015-06-29 01:14:56 +0530

    added support for both testing(using mongodb on local machine) and deploying (using mongolab on heroku) using same repo; added npm test command for testing using nodemon

--- dao.js
+++ dao.js
@@ -1,8 +1,17 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
+var mongouri = require('mongodb-uri');
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }; 
 
 var dbconfig = JSON.parse(fs.readFileSync('./config/dbconfig.json'))
-var connectionString = "mongodb://"+dbconfig.host+"/"+dbconfig.dbName
+
+// If MONGOLAB_URI config var is available (available in heroku), use it; else use the default config params
+// This ensures that the same repo can be used to test (using mongodb on the local machine) and deploy (on heroku; uses mongolab) 
+var connectionString = process.env.MONGOLAB_URI || "mongodb://"+dbconfig.host+"/"+dbconfig.dbName
+
+var mongooseuri = mongouri.formatMongoose(connectionString);
 
 mongoose.connect(connectionString, function (err) {
 	if(err){
