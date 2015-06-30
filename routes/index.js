97411ee0a8aab2a2e08287415d2c11a18ede8d0d
nautilX <mathewjoethomas@gmail.com>
2015-06-30 12:17:14 +0530

    removed test route

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,8 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 router.get('/', function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/../views/index.html'));
 })
-
-router.get('/test', function (req, res, next) {
-	res.sendFile(path.join(__dirname + '/../views/test.html'));
-})
-
 
 module.exports = router;
